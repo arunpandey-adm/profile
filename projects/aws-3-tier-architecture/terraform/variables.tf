8b8aa8d8dc3d47dfcfb8716f0b931b3aa0da0588
Arun Pandey <asp.dev@outlook.com>
2026-09-07 23:29:37 +0530

    Add Terraform variables for 3-tier project

---
+++ projects/aws-3-tier-architecture/terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "aws_region" {
+  description = "AWS region for the portfolio deployment"
+  type        = string
+  default     = "ap-south-1"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.20.0.0/16"
+}
